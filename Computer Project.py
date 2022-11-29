9b76e8e98eb52446add0603927bf06d3ec2bce15
Adit1230 <116501351+Adit1230@users.noreply.github.com>
2022-11-29 13:48:27 +0530

    Beautified main page and bound enter key to functions

--- Computer Project.py
+++ Computer Project.py
@@ -36,21 +36,26 @@ def init_login(From):
 
     Logo = ImageTk.PhotoImage(Image.open('Bank Logo.jpg'))
     login_win.iconphoto(False, Logo)
-    
-    lbl_login=Label(login_win,text="Login",justify='center',font = ('Arial ' , 25), fg='#05f').place(relx=0.5,rely=0.35,anchor='c')
 
-    lbl_username=Label(login_win,text="Username:", font = ('Arial' , 15)).place(relx=0.42,rely=0.4 , anchor='c')
+    Bg = ImageTk.PhotoImage(Image.open('Bank Background.jpg').resize((1500,750)))
+    lbl_bg=Label(login_win, image=Bg)
+    lbl_bg.pack()
+
+    lbl_login=Label(login_win,text="Login",justify='center',font = ('Arial ' , 25), fg='#0f0',bg='#054175').place(relx=0.5,rely=0.35,anchor='c')
+
+    lbl_username=Label(login_win,text="Username:", font = ('Arial' , 15), fg='#ffffff', bg='#054175').place(relx=0.42,rely=0.4 , anchor='c')
     tb_username=Entry(login_win ,font = ('Arial' , 15))
     tb_username.place(relx=0.54,rely=0.4,anchor ='c')
     
-    lbl_password=Label(login_win,text="Password:",font = ('Arial' , 15)).place(relx=0.42,rely=0.45 , anchor='c')
+    lbl_password=Label(login_win,text="Password:",font = ('Arial' , 15), fg='#ffffff', bg='#054175').place(relx=0.42,rely=0.45 , anchor='c')
     tb_password=Entry(login_win,show='*',font = ('Arial' , 15))
     tb_password.place(relx=0.54,rely=0.45,anchor ='c')
     
     btn_login=Button(text="Login",command=login,font = ('Arial' , 15))
     btn_login.place(relx=0.5 , rely=0.51,anchor = 'c')
+    login_win.bind('<Return>' , lambda event:login())
 
-    lbl_signup=Label(login_win,text="Don't have an account?",font = ('Arial' , 15), fg='#a00').place(relx=0.5 , rely = 0.56 , anchor = 'c')
+    lbl_signup=Label(login_win,text="Don't have an account?",font = ('Arial' , 15), fg='#c00', bg='#085B9D').place(relx=0.5 , rely = 0.56 , anchor = 'c')
     btn_signup=Button(text="Signup",font = ('Arial' , 15),command=lambda: init_signup('login'))
     btn_signup.place(relx=0.5 , rely = 0.61, anchor = 'c')
 
@@ -74,35 +79,43 @@ def init_signup(From):
     signup_win.title("Sign-up Page")
     signup_win.geometry('1500x750')
 
-    frame_ui = Frame(signup_win)
+    Logo = ImageTk.PhotoImage(Image.open('Bank Logo.jpg'))
+    signup_win.iconphoto(False, Logo)
+
+    Bg = ImageTk.PhotoImage(Image.open('Bank Background.jpg').resize((1500,750)))
+    lbl_bg=Label(signup_win, image=Bg)
+    lbl_bg.pack()
+    
+    frame_ui = Frame(signup_win, bg='#055a86')
     frame_ui.place(relx=0.375,rely=0.25)
 
-    lbl_createacnt=Label(frame_ui,text= 'Create an Account', font = ('Arial' , 25), fg='#05f').grid(row=0,column=0,columnspan=2)
+    lbl_createacnt=Label(frame_ui,text= 'Create an Account', font = ('Arial' , 25), fg='#0f0', bg='#055a86').grid(row=0,column=0,columnspan=2)
 
-    lbl_name=Label(frame_ui,text='Enter Name:', font = ('Arial ' , 15)).grid(row=1,column=0,pady=5)
+    lbl_name=Label(frame_ui,text='Enter Name:', font = ('Arial ' , 15), bg='#055a86').grid(row=1,column=0,pady=5)
     tb_name=Entry(frame_ui, width=10, font = ('Arial' , 15))
-    tb_name.grid(row=1, column=1,pady=5)
+    tb_name.grid(row=1, column=1, pady=5, padx=5)
 
-    lbl_username=Label(frame_ui, text= 'Enter Username:', font = ('Arial' , 15)).grid(row=2,column=0,pady=5)
+    lbl_username=Label(frame_ui, text= 'Enter Username:', font = ('Arial' , 15), bg='#055a86').grid(row=2,column=0,pady=5)
     tb_username=Entry(frame_ui, width=10, font = ('Arial' , 15))
-    tb_username.grid(row=2,column=1,pady=5)
+    tb_username.grid(row=2, column=1, pady=5, padx=5)
 
-    lbl_password=Label(frame_ui, text= 'Enter Password:', font = ('Arial' , 15)).grid(row=3,column=0,pady=5)
+    lbl_password=Label(frame_ui, text= 'Enter Password:', font = ('Arial' , 15), bg='#055a86').grid(row=3,column=0,pady=5)
     tb_password=Entry(frame_ui,width=10,show='*', font = ('Arial' , 15))
-    tb_password.grid(row=3,column=1,pady=5)
+    tb_password.grid(row=3, column=1, pady=5, padx=5)
 
-    lbl_reenterpass=Label(frame_ui,text='Re-enter password:', font = ('Arial' , 15)).grid(row=4,column=0,pady=5)
+    lbl_reenterpass=Label(frame_ui,text='Re-enter password:', font = ('Arial' , 15), bg='#055a86').grid(row=4,column=0,pady=5)
     tb_reenterpass=Entry(frame_ui,width=10,show='*', font = ('Arial' , 15))
-    tb_reenterpass.grid(row=4,column=1,pady=5)
+    tb_reenterpass.grid(row=4, column=1, pady=5, padx=5)
 
-    lbl_initdep=Label(frame_ui,text='Enter Initial Deposit:', font = ('Arial' , 15)).grid(row=5,column=0,pady=5)
+    lbl_initdep=Label(frame_ui,text='Enter Initial Deposit:', font = ('Arial' , 15), bg='#055a86').grid(row=5,column=0,pady=5)
     tb_initdep=Entry(frame_ui,width=10, font = ('Arial' , 15))
-    tb_initdep.grid(row=5,column=1,pady=5)
+    tb_initdep.grid(row=5, column=1, pady=5, padx=5)
 
     btn_signup=Button(frame_ui, text='Sign Up',command=signup, font = ('Arial' , 15))
-    btn_signup.grid(row=6,column=0,columnspan=2,pady=10)
+    btn_signup.grid(row=6, column=0, columnspan=2, pady=10)
+    signup_win.bind('<Return>' , lambda event:signup())
 
-    lbl_login=Label(frame_ui, text="Already have an account?", font = ('Arial' , 15), fg='#a00').grid(row=7,column=0,columnspan=2,pady=15)
+    lbl_login=Label(frame_ui, text="Already have an account?", font = ('Arial' , 15), fg='#a00', bg='#055a86').grid(row=7,column=0,columnspan=2,pady=15)
     btn_login=Button(frame_ui,text="Go back to login page", font = ('Arial' , 15), command=lambda: init_login('signup'))
     btn_login.grid(row=8,column=0,columnspan=2,pady=0)
 
@@ -112,6 +125,7 @@ def init_main(From):
     global main_win
     global user_list
     global frame_content
+    global frame_centering
     global lbl_balance
     
     if From=='login':
@@ -125,6 +139,13 @@ def init_main(From):
     main_win=Tk()
     main_win.title("Main page")
     main_win.geometry('1250x750')
+
+    Logo = ImageTk.PhotoImage(Image.open('Bank Logo.jpg'))
+    main_win.iconphoto(False, Logo)
+
+    Bg = ImageTk.PhotoImage(Image.open('Bank Background.jpg').resize((1500,750)))
+    lbl_bg=Label(main_win, image=Bg)
+    lbl_bg.place(relx=0,rely=0, anchor='nw')
 
     lbl_name=Label(main_win,text=("Name : "+user_list[1]), font = ('Arial' , 15))
     lbl_name.grid(row=0,column=0)
@@ -153,9 +174,12 @@ def init_main(From):
     btn_history=Button(frame_btn,text='Transaction History', width=15, font = ('Arial' , 15), command=lambda: display_tab('transaction history') )
     btn_history.grid(row=3,column=0)
     
-    frame_content=Frame(main_win,width=750,height=500,pady=10,padx=20)
+    frame_content=Frame(main_win,width=750,height=500,pady=10,padx=0,bg='#7fd2df')
     frame_content.grid(row=4,column=1)
     frame_content.grid_propagate(False)
+
+    frame_centering=Frame(frame_content, bg='#7fd2df')
+    frame_centering.place(relx=0.5,rely=0,anchor='n')
 
     for i in eval(user_list[6]):
         messagebox.showinfo("Money Transferred", i)
@@ -170,56 +194,59 @@ def init_main(From):
     conn.close()
     
 def display_tab(tab):
-    for widgets in frame_content.winfo_children():
+    for widgets in frame_centering.winfo_children():
         widgets.destroy()
 
     if tab=='transfer':
         global tb_user
         global tb_transfer
 
-        lbl_transfer=Label(frame_content, text="Transfer", font = ('Arial' , 15), pady=20).grid(row=0,column=0,columnspan=2)
+        lbl_transfer=Label(frame_centering, text="Transfer", font = ('Arial' , 20, 'bold'), bg='#7fd2df', fg='#000000', pady=20).grid(row=0,column=0,columnspan=2)
         
-        lbl_user=Label(frame_content,text='Enter username of recepient :', font = ('Arial' , 15)).grid(row=1,column=0)
-        tb_user=Entry(frame_content, font = ('Arial' , 15))
+        lbl_user=Label(frame_centering,text='Enter username of recepient :', font = ('Arial' , 15), bg='#7fd2df', fg='#000000').grid(row=1,column=0)
+        tb_user=Entry(frame_centering, font = ('Arial' , 15))
         tb_user.grid(row=1,column=1)
 
-        lbl_amt=Label(frame_content,text='Enter amount to be transferred :', font = ('Arial' , 15)).grid(row=2,column=0)
-        tb_transfer=Entry(frame_content, font = ('Arial' , 15))
+        lbl_amt=Label(frame_centering,text='Enter amount to be transferred :', font = ('Arial' , 15), bg='#7fd2df', fg='#000000').grid(row=2,column=0)
+        tb_transfer=Entry(frame_centering, font = ('Arial' , 15))
         tb_transfer.grid(row=2,column=1)
 
-        btn_transfer = Button(frame_content , text = 'Transfer ', font = ('Arial' , 15), command = lambda: transfer(tb_transfer.get(), tb_user.get()))
-        btn_transfer.grid(row=3, column = 0 , columnspan = 2)
+        btn_transfer = Button(frame_centering , text = 'Transfer ', font = ('Arial',15), command = lambda: transfer(tb_transfer.get(), tb_user.get()))
+        btn_transfer.grid(row=3, column = 0 , columnspan = 2, pady=10)
+        main_win.bind('<Return>' , lambda event:transfer(tb_transfer.get(), tb_user.get()))
 
     elif tab=='withdraw':
         global tb_withdraw
 
-        lbl_withdraw=Label(frame_content, text="Withdraw", font = ('Arial' , 15), pady=20).grid(row=0,column=0,columnspan=2)
+        lbl_withdraw=Label(frame_centering, text="Withdraw", font = ('Arial' , 20, 'bold'), bg='#7fd2df', fg='#000000', pady=20).grid(row=0,column=0,columnspan=2)
         
-        lbl_amount= Label(frame_content , text = 'Enter amount to be withdrawn : ', font = ('Arial' , 15) ).grid(row=1 , column=0)
-        tb_amount = Entry(frame_content, font = ('Arial' , 15))
+        lbl_amount= Label(frame_centering , text = 'Enter amount to be withdrawn : ', font = ('Arial',15), bg='#7fd2df', fg='#000000').grid(row=1,column=0)
+        tb_amount = Entry(frame_centering, font = ('Arial' , 15))
         tb_amount.grid(row=1,column=1)
-        btn_withdraw = Button(frame_content , text = 'Withdraw', font = ('Arial' , 15) , command = lambda: withdraw(tb_amount.get()))
-        btn_withdraw.grid(row=2, column=0, columnspan = 2)
+        btn_withdraw = Button(frame_centering , text = 'Withdraw', font = ('Arial' , 15) , command = lambda: withdraw(tb_amount.get()))
+        btn_withdraw.grid(row=2, column=0, columnspan = 2, pady=10)
+        main_win.bind('<Return>' , lambda event:withdraw(tb_amount.get()))
 
     elif tab=='deposit':
         global tb_deposit
 
-        lbl_deposit=Label(frame_content, text="Deposit", font = ('Arial' , 15), pady=20).grid(row=0,column=0,columnspan=2)
+        lbl_deposit=Label(frame_centering, text="Deposit", font = ('Arial' , 20, 'bold'), bg='#7fd2df', fg='#000000', pady=20).grid(row=0,column=0,columnspan=2)
         
-        lbl_amount= Label(frame_content , text = 'Enter amount to be deposited : ', font = ('Arial' , 15)).grid(row=1 , column=0)
-        tb_amount = Entry(frame_content, font = ('Arial' , 15))
+        lbl_amount= Label(frame_centering , text = 'Enter amount to be deposited : ', font = ('Arial' , 15), bg='#7fd2df', fg='#000000').grid(row=1 , column=0)
+        tb_amount = Entry(frame_centering, font = ('Arial' , 15))
         tb_amount.grid(row=1,column=1)
-        btn_deposit = Button(frame_content , text = 'Deposit', font = ('Arial' , 15), command = lambda: deposit(tb_amount.get()))
-        btn_deposit.grid(row=2, column=0, columnspan = 2)
+        btn_deposit = Button(frame_centering , text = 'Deposit', font = ('Arial' , 15), command = lambda: deposit(tb_amount.get()))
+        btn_deposit.grid(row=2, column=0, columnspan = 2, pady=10)
+        main_win.bind('<Return>' , lambda event:deposit(tb_amount.get()))
 
     elif tab=='transaction history':
-        lbl_TransHist=Label(frame_content, text="Transaction History", font = ('Arial' , 15), pady=20).grid(row=0,column=0)
+        lbl_TransHist=Label(frame_centering, text="Transaction History", font = ('Arial' , 20, 'bold'), bg='#7fd2df', fg='#000000', pady=20).grid(row=0,column=0)
                 
         style = ttk.Style()
         style.configure("Style1.Treeview", font=('Arial', 15))
         style.configure("Style1.Treeview.Heading", font=('Arial',15,'bold'))
         
-        table = ttk.Treeview(frame_content, style="Style1.Treeview")
+        table = ttk.Treeview(frame_centering, style="Style1.Treeview")
 
         table['columns'] = ("Date", "Transaction", "Change in balance", "Total balance")
 
@@ -241,6 +268,8 @@ def display_tab(tab):
             table.insert(parent='', index=0, iid = len(user_list[5])-i, text='', values = user_list[5][i])
 
         table.grid(row=1,column=0)
+
+        main_win.bind('<Return>', lambda event:display_tab('transaction history'))
     
     return()
 
@@ -267,7 +296,8 @@ def login():
             init_main('login')
         else:
             messagebox.showwarning("Wrong username or password","Username and password don't match")
-    
+
+   
     conn.commit()
     conn.close()
     return()
@@ -353,6 +383,9 @@ def withdraw(amount):
             auth_win=Toplevel(main_win)
             auth_win.title("Authentication")
 
+            Logo = ImageTk.PhotoImage(Image.open('Bank Logo.jpg'))
+            auth_win.iconphoto(False, Logo)
+
             def confirm_pass():
                     if tb_pass.get()==user_list[3]:
                         auth_win.destroy()
@@ -381,6 +414,7 @@ def withdraw(amount):
             tb_pass.grid(row=1,column=1)
             btn_auth=Button(auth_win,text="Confirm",command=confirm_pass)
             btn_auth.grid(row=2,column=0,columnspan=2)
+            auth_win.bind('<Return>', lambda event:confirm_pass())
 
             auth_win.mainloop()
                 
@@ -413,6 +447,9 @@ def transfer(amount,user):
         else:
             auth_win=Toplevel(main_win)
             auth_win.title("Authentication")
+
+            Logo = ImageTk.PhotoImage(Image.open('Bank Logo.jpg'))
+            auth_win.iconphoto(False, Logo)
 
             def confirm_pass():
                 if tb_pass.get()==user_list[3]:
@@ -463,6 +500,7 @@ def transfer(amount,user):
             tb_pass.grid(row=1,column=1)
             btn_auth=Button(auth_win,text="Confirm",command=confirm_pass)
             btn_auth.grid(row=2,column=0,columnspan=2)
+            auth_win.bind('<Return>', lambda event:confirm_pass())
 
             auth_win.mainloop()
             
